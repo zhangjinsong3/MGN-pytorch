54789ca816f7a68a285a83e34d23a583f76ddc5a
jingsongzhang@sf-express.com <01376022@sf-express.com>
2019-06-28 16:19:04 +0800

    add train boxes dataset script

---
+++ scripts/train_boxes.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Note: 200 server has no enough shared memory, set nThread and batch smaller.
+CUDA_VISIBLE_DEVICES=6,7 python main.py  \
+--datadir ../../Opensource_datasets/Boxes  \
+--data_train Boxes \
+--data_test Boxes \
+--num_classes 3000 \
+--batchid 8  \
+--batchtest 16  \
+--test_every 20  \
+--epochs 160  \
+--decay_type step_120_140  \
+--loss 1*CrossEntropy+2*Triplet  \
+--margin 1.2  \
+--re_rank  \
+--random_erasing  \
+--save Boxes_MGN_adam_margin_1.2  \
+--nThread 1 \
+--nGPU 2   \
+--lr 2e-4  \
+--optimizer ADAM \
+--save_models \
+--reset
